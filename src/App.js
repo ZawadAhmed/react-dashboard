0ab83bcef718d2eef21d17649f078dcc51defa2e
ZawadAhmed <zawadwasik@gmail.com>
2023-09-11 14:38:09 +0800

    Adding of Form feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import { useState } from "react";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Topbar from "./components/global/topbar";
 import Sidebar from "./components/global/sidebar";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 
+//routes 
+import Calendar from "./pages/calendar";
+//import Pie from "./pages/pie";
+//import Bar from "./pages/bar";
+//import Line from "./pages/line";
+//import FAQ from "./pages/faq";
+//import Invoices from "./pages/invoices";
+import Dashboard from "./pages/dashboard";
+//import Team from "./pages/team";
+import Form from "./pages/form";
+//import Geography from "./pages/geography";
+//import Contacts from "./pages/contacts";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -19,8 +31,21 @@ function App() {
           <main className="content">
             <Topbar setIsSidebar={setIsSidebar} />
          
-            <Routes>
-            </Routes>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+              {/*<Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+           
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/faq" element={<FAQ />} />
+             
+              <Route path="/geography" element={<Geography />} /> */}
+               <Route path="/calendar" element={<Calendar />} />
+               <Route path="/form" element={<Form />} />
+          </Routes>
            
           </main>
         </div>
