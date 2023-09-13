3333c271048df96fc4a3899dc1856a6c764c4550
ZawadAhmed <zawadwasik@gmail.com>
2023-09-13 16:59:13 +0800

    Team page content

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ import Calendar from "./pages/calendar";
 //import Pie from "./pages/pie";
 //import Bar from "./pages/bar";
 //import Line from "./pages/line";
-//import FAQ from "./pages/faq";
-//import Invoices from "./pages/invoices";
+import FAQ from "./pages/faq";
+import Invoices from "./pages/invoices";
 import Dashboard from "./pages/dashboard";
-//import Team from "./pages/team";
+import Team from "./pages/team";
 import Form from "./pages/form";
-//import Geography from "./pages/geography";
-//import Contacts from "./pages/contacts";
+import Geography from "./pages/geography";
+import Contacts from "./pages/contacts";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -33,18 +33,19 @@ function App() {
          
           <Routes>
             <Route path="/" element={<Dashboard />} />
-              {/*<Route path="/team" element={<Team />} />
-              <Route path="/contacts" element={<Contacts />} />
-              <Route path="/invoices" element={<Invoices />} />
+              {/*
+            
            
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
-              <Route path="/line" element={<Line />} />
+              <Route path="/line" element={<Line />} />*/}
               <Route path="/faq" element={<FAQ />} />
-             
-              <Route path="/geography" element={<Geography />} /> */}
-               <Route path="/calendar" element={<Calendar />} />
-               <Route path="/form" element={<Form />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/geography" element={<Geography />} />
           </Routes>
            
           </main>
